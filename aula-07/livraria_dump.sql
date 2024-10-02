da5845d68bfc3f5dc9bdc73c76951e83065de43d
unknown <daniellsilva@outlook.com>
2024-10-01 21:39:05 -0300

    Correção da aula 07

--- aula-07/livraria_dump.sql
+++ aula-07/livraria_dump.sql
@@ -19,6 +19,9 @@
 -- Table structure for table `livro`
 --
 
+CREATE DATABASE IF NOT EXISTS `livraria`;
+USE `livraria`;
+
 DROP TABLE IF EXISTS `livro`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
