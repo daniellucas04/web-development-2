6e269120332a7e9f1ade0e05cf455df821d5cd1a
unknown <daniellsilva@outlook.com>
2024-11-20 00:12:42 -0300

    Aula 13 - Banco de dados

--- aula-13/sch_dump.sql
+++ aula-13/sch_dump.sql
@@ -1,8 +1,9 @@
--- MariaDB dump 10.19  Distrib 10.4.32-MariaDB, for Win64 (AMD64)
+/*M!999999\- enable the sandbox mode */ 
+-- MariaDB dump 10.19-11.5.2-MariaDB, for Win64 (AMD64)
 --
 -- Host: localhost    Database: sch
 -- ------------------------------------------------------
--- Server version	10.4.32-MariaDB
+-- Server version	11.5.2-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -13,7 +14,7 @@
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*M!100616 SET @OLD_NOTE_VERBOSITY=@@NOTE_VERBOSITY, NOTE_VERBOSITY=0 */;
 
 CREATE DATABASE `sch`;
 USE `sch`;
@@ -38,8 +39,45 @@ CREATE TABLE `departments` (
 
 LOCK TABLES `departments` WRITE;
 /*!40000 ALTER TABLE `departments` DISABLE KEYS */;
-INSERT INTO `departments` VALUES (1,'Administração');
+INSERT INTO `departments` VALUES
+(1,'Administração');
 /*!40000 ALTER TABLE `departments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `requests`
+--
+
+DROP TABLE IF EXISTS `requests`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `requests` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_department` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `description` mediumtext NOT NULL,
+  `priority` enum('L','M','H','U') NOT NULL DEFAULT 'L',
+  `id_technician` int(11) NOT NULL,
+  `limit_date` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_department` (`id_department`),
+  KEY `id_user` (`id_user`),
+  KEY `id_technician` (`id_technician`),
+  CONSTRAINT `requests_ibfk_1` FOREIGN KEY (`id_department`) REFERENCES `departments` (`id`),
+  CONSTRAINT `requests_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`),
+  CONSTRAINT `requests_ibfk_3` FOREIGN KEY (`id_technician`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `requests`
+--
+
+LOCK TABLES `requests` WRITE;
+/*!40000 ALTER TABLE `requests` DISABLE KEYS */;
+INSERT INTO `requests` VALUES
+(1,1,5,'O computador 3 está com defeito. Verificar possível problema no HD','M',7,'2024-11-20 18:00:00');
+/*!40000 ALTER TABLE `requests` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -66,7 +104,11 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (4,'admin','admin@master.com','$argon2i$v=19$m=65536,t=4,p=1$LkdYcFNBVXdhTmprS2pjeA$+V17FFSezZfhUw0xh78VcWvl70tqn9juqVr5AiMPg9I','F'),(5,'admin','admin@email.com','$argon2i$v=19$m=65536,t=4,p=1$RXIyUFJFNHhKWG5UdFRtTg$Fx2w9JQAf7t1x9D6pICSrgIS/4f+W13LwlgHtlF2ZuY','F'),(6,'admin','admin@google.com','$argon2i$v=19$m=65536,t=4,p=1$enU2ZlZCV2pQV0R1dVZFaA$KjYyTpiCiPzq/jf/8Y/CrggvLfPPg+b8SLUrU7ETzF0','F'),(7,'admin','admin@meta.com','$argon2i$v=19$m=65536,t=4,p=1$OEtTdHJqaXJLTElVZzlKaw$uciOFInPgmbqRWjn6ggK6Zb3e19Ic+mlbNxR6O5/ATk','T');
+INSERT INTO `users` VALUES
+(4,'admin','admin@master.com','$argon2i$v=19$m=65536,t=4,p=1$LkdYcFNBVXdhTmprS2pjeA$+V17FFSezZfhUw0xh78VcWvl70tqn9juqVr5AiMPg9I','F'),
+(5,'admin','admin@email.com','$argon2i$v=19$m=65536,t=4,p=1$RXIyUFJFNHhKWG5UdFRtTg$Fx2w9JQAf7t1x9D6pICSrgIS/4f+W13LwlgHtlF2ZuY','F'),
+(6,'admin','admin@google.com','$argon2i$v=19$m=65536,t=4,p=1$enU2ZlZCV2pQV0R1dVZFaA$KjYyTpiCiPzq/jf/8Y/CrggvLfPPg+b8SLUrU7ETzF0','F'),
+(7,'admin','admin@meta.com','$argon2i$v=19$m=65536,t=4,p=1$OEtTdHJqaXJLTElVZzlKaw$uciOFInPgmbqRWjn6ggK6Zb3e19Ic+mlbNxR6O5/ATk','T');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -77,6 +119,6 @@ UNLOCK TABLES;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*M!100616 SET NOTE_VERBOSITY=@OLD_NOTE_VERBOSITY */;
 
--- Dump completed on 2024-11-19 22:05:56
+-- Dump completed on 2024-11-20  0:12:07
